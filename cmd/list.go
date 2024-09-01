70009fffd40f286ca7cf270ac9879233c6c86841
TheRedScreen <theredscreen@t-online.de>
2024-09-01 16:20:17 +0200

    Add list all example to list command

--- cmd/list.go
+++ cmd/list.go
@@ -9,9 +9,10 @@ var listCmd = &cobra.Command{
 	Use:   "list [<status>]",
 	Short: "List all tasks",
 	Long:  "List all tasks. You can filter tasks by status.",
-	Example: `task-tracker list todo
-    task-tracker list in-progress
-    task-tracker list done`,
+	Example: `task-tracker list
+task-tracker list todo
+task-tracker list in-progress
+task-tracker list done`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) > 0 {
 			return task.ListTasks(args[0])
